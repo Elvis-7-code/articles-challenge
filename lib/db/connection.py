190c541e22999f982d88c201328ba10ca6a16dc9
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-26 23:45:30 +0300

    changes@connection.py

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -1,6 +1,9 @@
 import sqlite3
+CONN = sqlite3.connect('db/articles.db')
+CURSOR = CONN.cursor()
 def get_connection():
     conn = sqlite3.connect('articles.db')
     conn.row_factory = sqlite3.Row  # This allows us to access columns by name
-    return conn
-def 
+    return CONN
+def get_cursor():
+    return CURSOR
