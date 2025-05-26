21d0d14c638c77383b3ccd082fd470fdaee59d52
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-26 20:47:00 +0300

    articles_sql

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS magazines (
     name TEXT NOT NULL,
     category TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS articles (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    author_id INTEGER,
+    magazine_id INTEGER,
+    FOREIGN KEY(author_id) REFERENCES authors(id),
+    FOREIGN KEY(magazine_id) REFERENCES magazines(id)
+);
